60d71ec311acb60e06a5c088983c1590b99fb1d1
fengyin-solo <1318978787@qq.com>
2026-06-19 09:33:30 +0800

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,3 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-export default defineConfig({ plugins: [vue()], server: { port: 5180, open: true, proxy: { '/api': 'http://localhost:8002' } } })
+export default defineConfig({ plugins: [vue()], server: { port: 5180, open: false, proxy: { '/api': 'http://localhost:8002' } } })
